712f728df1548a0d224c4fc54a9c0de1c8b3774a
ESLS <lorashin1903@gmail.com>
2025-04-01 21:08:05 +0900

    excercise 1 updated

--- ex-01.c
+++ ex-01.c
@@ -2,3 +2,12 @@
     1. 0~127까지의 ascii code를 출력하는 프로그램을 작성하시오.
     * ascii table
 */
+#include <stdio.h>
+
+int main(void)
+{
+ int ascii;
+ for (ascii=0; ascii<=100; ascii++)
+    {printf("%c", ascii);}
+    return 0;
+}
